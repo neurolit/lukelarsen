13211f76610f44fd3c9a34431627f59a9c25cefa
Luke Larsen <luke@lukelarsen.com>
2013-02-21 21:03:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lukelarsen::Application.config.secret_token = '71c8f937aef2fce695b8920c0113aa37d31b658680c3667d7b554fab5c2a8dc793260f8a5341199d64c23018f013a61541ea28d7a20c838b8c7321e52e7d6e91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lukelarsen::Application.config.session_store :cookie_store, key: '_lukelarsen_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lukelarsen::Application.config.session_store :active_record_store
